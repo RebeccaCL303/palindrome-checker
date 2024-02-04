a50ae5a4e212c86a3eb2c4a630c9d8ff3fdbd433
RebeccaCL303 <126423309+RebeccaCL303@users.noreply.github.com>
2024-02-04 14:27:01 -0600

    fixed js to pass freecodecamp tests

--- src/index.js
+++ src/index.js
@@ -2,29 +2,25 @@ const checkBtn = document.getElementById("check-btn");
 const textInput = document.getElementById("text-input");
 const resultElement = document.getElementById("result");
 
-let result;
+let result = "";
 
 const getResult = () => {
- palindromeTestStr = result.split("").reverse().toString().replaceAll(",", "");
- if (palindromeTestStr === result) {
-  resultElement.innerHTML = `<strong>${result}</strong> is a palindrome`;
+ const str = textInput.value;
+ const palindromeTestStr = str.split("").reverse().join("");
+ if (palindromeTestStr === str) {
+  resultElement.innerHTML = `${str} is a palindrome`;
  } else {
-  resultElement.innerHTML = `<strong>${result}</strong> is not a palindrome`;
+  resultElement.innerHTML = `${str} is not a palindrome`;
  }
 };
 
 const handleSubmit = (event) => {
  event.preventDefault();
- if (result.length > 0) {
-  getResult();
- } else {
+ if (textInput.value.length === 0) {
   alert("Please input a value");
+ } else {
+  getResult();
  }
 };
 
-const handleChange = (event) => {
- result = event.target.value;
-};
-
-checkBtn.onclick = handleSubmit;
-textInput.oninput = handleChange;
+checkBtn.addEventListener("click", handleSubmit);
