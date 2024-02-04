5cb1db64b131b0caf9419e5040ac7ef302caad7e
RebeccaCL303 <126423309+RebeccaCL303@users.noreply.github.com>
2024-02-04 15:10:12 -0600

    changed getResult function to only compare letters

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ const checkBtn = document.getElementById("check-btn");
 const textInput = document.getElementById("text-input");
 const resultElement = document.getElementById("result");
 
-let result = "";
-
 const getResult = () => {
  const str = textInput.value;
- const palindromeTestStr = str.split("").reverse().join("");
- if (palindromeTestStr === str) {
+ const charsToRemoveRegex = /[\S\W\d]/g;
+ const charsToRemove = str.match(charsToRemoveRegex);
+ const palindromeTestStr = str.replace(charsToRemove, "").toLowerCase();
+ const reverseStr = str
+  .replace(charsToRemove, "")
+  .toLowerCase()
+  .split("")
+  .reverse()
+  .join("");
+ if (reverseStr === palindromeTestStr) {
   resultElement.innerHTML = `${str} is a palindrome`;
  } else {
   resultElement.innerHTML = `${str} is not a palindrome`;
